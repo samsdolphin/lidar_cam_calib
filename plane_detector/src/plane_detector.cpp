4b8b00981a88a6506c637b402ab7fb19f50a3517
samsdolphin <liuxiyuan95@gmail.com>
2020-10-14 20:37:53 +0800

    restructure the plane detector

--- plane_detector/src/plane_detector.cpp
+++ plane_detector/src/plane_detector.cpp
@@ -10,6 +10,7 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <opencv2/core/core.hpp>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/SVD>
 #include <ceres/ceres.h>
@@ -17,16 +18,10 @@
 
 using namespace std;
 using namespace Eigen;
+using namespace cv;
 
 typedef pcl::PointXYZI PointType;
-pcl::PointCloud<PointType>::Ptr pc_surf(new pcl::PointCloud<PointType>);
-string address = "/home/sam/catkin_ws/src/lidar_cam_calib/plane_detector/mid40/hkust/s2.json";
-
 #define PI 3.14159265
-int max_svd_it = 30;
-double inlier_ratio = 0.95;
-double max_svd_val = 0.9;
-double max_radius = 0.2;
 
 pcl::PointCloud<PointType> read_pointcloud(std::string path)
 {
@@ -49,62 +44,53 @@ pcl::PointCloud<PointType> read_pointcloud(std::string path)
 	return pc;
 }
 
-pcl::PointCloud<PointType>::Ptr append_cloud(pcl::PointCloud<PointType>::Ptr pc1, pcl::PointCloud<PointType> pc2)
-{
-    size_t size1 = pc1->points.size();
-    size_t size2 = pc2.points.size();
-    pc1->points.resize(size1 + size2);
-    for (size_t i = size1; i < size1 + size2; i++)
-    {
-        pc1->points[i].x = pc2.points[i - size1].x;
-        pc1->points[i].y = pc2.points[i - size1].y;
-        pc1->points[i].z = pc2.points[i - size1].z;
-    }
-    return pc1;
-}
-
-void surf_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
-{
-    pcl::PointCloud<PointType>::Ptr pc(new pcl::PointCloud<PointType>);
-    pcl::fromROSMsg(*msg, *pc);
-    pc_surf = append_cloud(pc_surf, *pc);
-
-    ofstream file_w;
-    file_w.open(address, std::ofstream::app);
-    for (size_t i = 0; i < pc->points.size(); i++)
-        file_w << pc->points[i].x << "\t" << pc->points[i].y << "\t" << pc->points[i].z << "\n";
-    file_w.close();
-}
-
 double compute_inlier(std::vector<double> residuals, double ratio)
 {
     std::sort(residuals.begin(), residuals.end());
-    // cout<<"inlier "<<residuals[floor(ratio * residuals.size())]<<endl;
-    // cout<<"max "<<residuals[residuals.size()-1]<<endl;
     return residuals[floor(ratio * residuals.size())];
 }
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "plane_detector");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
     ros::Publisher pub_origin = nh.advertise<sensor_msgs::PointCloud2>("/origin_surf", 10000);
     ros::Publisher pub_rough = nh.advertise<sensor_msgs::PointCloud2>("/rough_surf", 10000);
     ros::Publisher pub_filt = nh.advertise<sensor_msgs::PointCloud2>("/filt_surf", 10000);
-    ros::Publisher pub_nor = nh.advertise<visualization_msgs::MarkerArray>("svd_nor", 0);
-    ros::Subscriber sub_surf = nh.subscribe<sensor_msgs::PointCloud2>("/pc2_surfaceN", 10000, surf_callback);
+    ros::Publisher pub_nor = nh.advertise<visualization_msgs::MarkerArray>("svd_nor", 10000);
 
-    pcl::PointCloud<PointType>::Ptr pc_src(new pcl::PointCloud<PointType>);
-    *pc_src = read_pointcloud("/home/sam/catkin_ws/src/lidar_cam_calib/plane_detector/mid40/hkust/s2.json");
+    string pointcloud_path, boundary_param;
+    cv::Mat boundary;
+    int max_SVD_iteration;
+    double inlier_SVD_ratio, max_SVD_value, max_circle_radius;
+    
+    nh.getParam("pointcloud_path", pointcloud_path);
+    nh.getParam("boundary_param", boundary_param);
+    nh.getParam("max_SVD_iteration", max_SVD_iteration);
+    nh.getParam("inlier_SVD_ratio", inlier_SVD_ratio);
+    nh.getParam("max_SVD_value", max_SVD_value);
+    nh.getParam("max_circle_radius", max_circle_radius);
+
+    cv::FileStorage param_reader(boundary_param, cv::FileStorage::READ);
+    param_reader["boundary"] >> boundary;
+
     pcl::PointCloud<PointType>::Ptr pc_rough(new pcl::PointCloud<PointType>);
+    pcl::PointCloud<PointType>::Ptr pc_src(new pcl::PointCloud<PointType>);
+    *pc_src = read_pointcloud(pointcloud_path);
+    
     pc_rough->points.resize(1e8);
     size_t cnt = 0;
 
     for (size_t i = 0; i < pc_src->points.size(); i++)
     {
-        if (pc_src->points[i].z > -0.8 && pc_src->points[i].z < 0 &&
-            pc_src->points[i].y > 0.3 && pc_src->points[i].y < 1.3 &&
-            pc_src->points[i].x > 3 && pc_src->points[i].x < 5)
+        if (
+            pc_src->points[i].x > boundary.at<double>(0, 0) &&
+            pc_src->points[i].x < boundary.at<double>(0, 1) &&
+            pc_src->points[i].y > boundary.at<double>(1, 0) &&
+            pc_src->points[i].y < boundary.at<double>(1, 1) &&
+            pc_src->points[i].z > boundary.at<double>(2, 0) &&
+            pc_src->points[i].z < boundary.at<double>(2, 1)
+            )
             {
                 pc_rough->points[cnt].x = pc_src->points[i].x;
                 pc_rough->points[cnt].y = pc_src->points[i].y;
@@ -123,7 +109,7 @@ int main(int argc, char** argv)
         candidates.push_back(tmp);
     }
 
-    for (int it = 0; it < max_svd_it; it++)
+    for (int it = 0; it < max_SVD_iteration; it++)
     {
         center.setZero();
         size_t pt_size = candidates.size();
@@ -147,7 +133,7 @@ int main(int argc, char** argv)
             double tmp = svd_nor.dot(candidates[i] - center);
             residuals.push_back(abs(tmp));
         }
-        double rej_val = compute_inlier(residuals, inlier_ratio);
+        double rej_val = compute_inlier(residuals, inlier_SVD_ratio);
 
         for (size_t i = 0; i < pt_size; i++)
         {
@@ -172,8 +158,8 @@ int main(int argc, char** argv)
         marker.scale.y = 0.05;
         marker.scale.z = 0.1;
         marker.color.a = 1.0;
-        marker.color.r = cos(PI*it/max_svd_it);
-        marker.color.g = sin(PI*it/max_svd_it);
+        marker.color.r = cos(PI*it/max_SVD_iteration);
+        marker.color.g = sin(PI*it/max_SVD_iteration);
         marker.color.b = 0.0;
         geometry_msgs::Point apoint;
         apoint.x = center(0);
@@ -187,10 +173,10 @@ int main(int argc, char** argv)
         marker_array.markers.push_back(marker);        
         pub_nor.publish(marker_array);
 
-        if (sig_val < max_svd_val)
+        if (sig_val < max_SVD_value)
         {
             for (size_t i = 0; i < pt_size; i++)
-                if ((candidates[i] - center).norm() < max_radius)
+                if ((candidates[i] - center).norm() < max_circle_radius)
                     new_can.push_back(candidates[i]);
 
             candidates.clear();
@@ -222,7 +208,7 @@ int main(int argc, char** argv)
     marker.header.frame_id = "/camera_init";
     marker.header.stamp = ros::Time();
     marker.ns = "my_namespace";
-    marker.id = max_svd_it;
+    marker.id = max_SVD_iteration;
     marker.type = visualization_msgs::Marker::ARROW;
     marker.action = visualization_msgs::Marker::ADD;
     marker.scale.x = 0.02;
---
+++ plane_detector/src/plane_extractor.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <ros/ros.h>
+
+using namespace std;
+using namespace Eigen;
+
+typedef pcl::PointXYZI PointType;
+pcl::PointCloud<PointType>::Ptr pc_surf(new pcl::PointCloud<PointType>);
+string data_path;
+
+pcl::PointCloud<PointType>::Ptr append_cloud(pcl::PointCloud<PointType>::Ptr pc1,
+                                             pcl::PointCloud<PointType> pc2)
+{
+    size_t size1 = pc1->points.size();
+    size_t size2 = pc2.points.size();
+    pc1->points.resize(size1 + size2);
+    for (size_t i = size1; i < size1 + size2; i++)
+    {
+        pc1->points[i].x = pc2.points[i - size1].x;
+        pc1->points[i].y = pc2.points[i - size1].y;
+        pc1->points[i].z = pc2.points[i - size1].z;
+    }
+    return pc1;
+}
+
+void surf_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
+{
+    pcl::PointCloud<PointType>::Ptr pc(new pcl::PointCloud<PointType>);
+    pcl::fromROSMsg(*msg, *pc);
+    pc_surf = append_cloud(pc_surf, *pc);
+
+    ofstream file_w;
+    file_w.open(data_path, std::ofstream::app);
+    for (size_t i = 0; i < pc->points.size(); i++)
+        file_w << pc->points[i].x << "\t" << pc->points[i].y << "\t" << pc->points[i].z << "\n";
+    file_w.close();
+}
+
+double compute_inlier(std::vector<double> residuals, double ratio)
+{
+    std::sort(residuals.begin(), residuals.end());
+    return residuals[floor(ratio * residuals.size())];
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "plane_extractor");
+    ros::NodeHandle nh("~");
+    ros::Subscriber sub_surf = nh.subscribe<sensor_msgs::PointCloud2>("/pc2_surfaceN", 10000, surf_callback);
+
+    nh.getParam("data_write_path", data_path);
+
+	ros::Rate loop_rate(1);
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
